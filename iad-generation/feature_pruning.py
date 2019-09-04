eb5471fe219fb204f685f4228576a358dbae9d52
MClarkTurner <mclarktu@fandm.edu>
2019-09-03 23:09:44 -0400

    test

--- iad-generation/feature_pruning.py
+++ iad-generation/feature_pruning.py
@@ -140,6 +140,7 @@ with tf.Session() as sess:
 # store rankings in a npy array
 depth, index, rank = [],[],[] 
 for i in range(len(r)):
+	print(r[i])
 	depth.append(np.full(len(r[i]), i))
 	index.append(np.arange(len(r[i])))
 	rank.append(r[i])
