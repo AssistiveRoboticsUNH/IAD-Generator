b222ef65a7d4593cef5246f4ba380e2c9fe02221
MClarkTurner <mclarktu@fandm.edu>
2019-09-03 22:41:09 -0400

    test

--- iad-generation/feature_pruning.py
+++ iad-generation/feature_pruning.py
@@ -32,91 +32,93 @@ conv_variables = tf.trainable_variables()#[v for v in tf.trainable_variables() i
 #https://jacobgil.github.io/deeplearning/pruning-deep-learning
 #https://stackoverflow.com/questions/43839431/tensorflow-how-to-replace-or-modify-gradient/43948872
 
-'''
+
 class Model:
 	def __init__(self):
-		self.activations = []
-'''
-def generate_full_model(input_ph, _weights, _biases, depth=4, separate_conv_layers=True):
-	'''Generates the activation map for a given input from a specific depth
-		-input_ph: the input placeholder, should have been defined using the 
-		  "get_input_placeholder" function
-		-_weights: weights used to convolve the input, defined in the 
-		  "get_variables" function
-		-_biases: biases used to convolve the input, defined in the 
-		  "get_variables" function
-		-depth: the depth at which the activation map should be extracted (an 
-		  int between 0 and 4)
-	'''
+		self.ranks = []
 
-	def conv3d(name, l_input, w, b):
-		# performs a 3d convolution
-		return tf.nn.bias_add(tf.nn.conv3d(l_input, w, strides=[1, 1, 1, 1, 1], padding='SAME'),b)
+	def generate_full_model(input_ph, _weights, _biases, depth=4, separate_conv_layers=True):
+		'''Generates the activation map for a given input from a specific depth
+			-input_ph: the input placeholder, should have been defined using the 
+			  "get_input_placeholder" function
+			-_weights: weights used to convolve the input, defined in the 
+			  "get_variables" function
+			-_biases: biases used to convolve the input, defined in the 
+			  "get_variables" function
+			-depth: the depth at which the activation map should be extracted (an 
+			  int between 0 and 4)
+		'''
 
-	def max_pool(name, l_input, k):
-		# performs a 2x2 max pool operation in 3 dimensions
-		return tf.nn.max_pool3d(l_input, ksize=[1, k, 2, 2, 1], strides=[1, k, 2, 2, 1], padding='SAME', name=name)
+		def conv3d(name, l_input, w, b):
+			# performs a 3d convolution
+			return tf.nn.bias_add(tf.nn.conv3d(l_input, w, strides=[1, 1, 1, 1, 1], padding='SAME'),b)
 
-	@tf.custom_gradient
-	def rank_layer(x):
-		def grad(dy):
-			x = tf.Print(x, [x], message="activ:", summarize=10)
-			dy = tf.Print(dy, [dy], message="grad:", summarize=10)
+		def max_pool(name, l_input, k):
+			# performs a 2x2 max pool operation in 3 dimensions
+			return tf.nn.max_pool3d(l_input, ksize=[1, k, 2, 2, 1], strides=[1, k, 2, 2, 1], padding='SAME', name=name)
 
-			ranks = tf.math.multiply(x, dy)
-			ranks = tf.Print(ranks, [ranks], message="rank_mul:", summarize=10)
+		@tf.custom_gradient
+		def rank_layer(x):
+			def grad(dy):
+				#x = tf.Print(x, [x], message="activ:", summarize=10)
+				#dy = tf.Print(dy, [dy], message="grad:", summarize=10)
 
-			ranks = tf.reduce_sum(ranks, axis=(0, 1, 2, 3)) #combine spatial and temporal points together
-			ranks = tf.Print(ranks, [ranks], message="rank_sum:", summarize=10)
+				#calculate the rank
+				ranks = tf.math.multiply(x, dy)
+				#ranks = tf.Print(ranks, [ranks], message="rank_mul:", summarize=10)
+				ranks = tf.reduce_sum(ranks, axis=(0, 1, 2, 3)) #combine spatial and temporal points together
+				#ranks = tf.Print(ranks, [ranks], message="rank_sum:", summarize=10)
 
-			prod_term = tf.cast(tf.reduce_prod(tf.shape(x)[:-1]), tf.float32)
-			ranks = tf.math.divide(ranks, prod_term) #normalization term
-			ranks = tf.Print(ranks, [ranks], message="rank_norm:", summarize=10)
+				#normalize the rank by the input size
+				prod_term = tf.cast(tf.reduce_prod(tf.shape(x)[:-1]), tf.float32)
+				ranks = tf.math.divide(ranks, prod_term) #normalization term
+				#ranks = tf.Print(ranks, [ranks], message="rank_norm:", summarize=10)
+				self.ranks.append(ranks)
 
-			return dy
+				return dy
 
-		#self.activations.append(x)
-		return tf.identity(x), grad
+			#self.activations.append(x)
+			return tf.identity(x), grad
 
-	# Convolution Layer
-	conv1 = conv3d('conv1', input_ph, _weights['wc1'], _biases['bc1'])
-	conv1 = rank_layer(conv1)
-	conv1 = tf.nn.relu(conv1, 'relu1')
-	pool1 = max_pool('pool1', conv1, k=1)
+		# Convolution Layer
+		conv1 = conv3d('conv1', input_ph, _weights['wc1'], _biases['bc1'])
+		conv1 = rank_layer(conv1)
+		conv1 = tf.nn.relu(conv1, 'relu1')
+		pool1 = max_pool('pool1', conv1, k=1)
 
-	# Convolution Layer
-	conv2 = conv3d('conv2', pool1, _weights['wc2'], _biases['bc2'])
-	#conv2 = rank_layer(conv2)
-	conv2 = tf.nn.relu(conv2, 'relu2')
-	pool2 = max_pool('pool2', conv2, k=2)
+		# Convolution Layer
+		conv2 = conv3d('conv2', pool1, _weights['wc2'], _biases['bc2'])
+		#conv2 = rank_layer(conv2)
+		conv2 = tf.nn.relu(conv2, 'relu2')
+		pool2 = max_pool('pool2', conv2, k=2)
 
-	# Convolution Layer
-	conv3 = conv3d('conv3a', pool2, _weights['wc3a'], _biases['bc3a'])
-	conv3 = tf.nn.relu(conv3, 'relu3a')
-	pool3 = max_pool('pool3', conv3, k=2)
+		# Convolution Layer
+		conv3 = conv3d('conv3a', pool2, _weights['wc3a'], _biases['bc3a'])
+		conv3 = tf.nn.relu(conv3, 'relu3a')
+		pool3 = max_pool('pool3', conv3, k=2)
 
-	# Convolution Layer
-	conv4 = conv3d('conv4a', pool3, _weights['wc4a'], _biases['bc4a'])
-	conv4 = tf.nn.relu(conv4, 'relu4a')
-	pool4 = max_pool('pool4', conv4, k=2)
+		# Convolution Layer
+		conv4 = conv3d('conv4a', pool3, _weights['wc4a'], _biases['bc4a'])
+		conv4 = tf.nn.relu(conv4, 'relu4a')
+		pool4 = max_pool('pool4', conv4, k=2)
 
-	# Convolution Layer
-	conv5 = conv3d('conv5a', pool4, _weights['wc5a'], _biases['bc5a'])
-	conv5 = tf.nn.relu(conv5, 'relu5a')
-	pool5 = max_pool('pool5', conv5, k=2) 
+		# Convolution Layer
+		conv5 = conv3d('conv5a', pool4, _weights['wc5a'], _biases['bc5a'])
+		conv5 = tf.nn.relu(conv5, 'relu5a')
+		pool5 = max_pool('pool5', conv5, k=2) 
 
-	#flatten actviation map
-	flat_am = tf.layers.flatten(pool5)
+		#flatten actviation map
+		flat_am = tf.layers.flatten(pool5)
 
-	#dense layers
-	dense1 = tf.nn.bias_add(tf.matmul(flat_am, _weights['wd1']), _biases['bd1'])
-	dense2 = tf.nn.bias_add(tf.matmul(dense1, _weights['wd2']), _biases['bd2'])
-	out = tf.nn.bias_add(tf.matmul(dense2, _weights['out']), _biases['out'])
+		#dense layers
+		dense1 = tf.nn.bias_add(tf.matmul(flat_am, _weights['wd1']), _biases['bd1'])
+		dense2 = tf.nn.bias_add(tf.matmul(dense1, _weights['wd2']), _biases['bd2'])
+		out = tf.nn.bias_add(tf.matmul(dense2, _weights['out']), _biases['out'])
 
-	softmax = tf.nn.softmax(out)
-	classifcation = tf.argmax(softmax, axis = 1)
+		softmax = tf.nn.softmax(out)
+		classifcation = tf.argmax(softmax, axis = 1)
 
-	return classifcation, softmax, out
+		return classifcation, softmax, out
 
 
 
@@ -135,7 +137,8 @@ class_op, softmax_op, pred_op = model.generate_full_model(input_placeholder, wei
 
 
 '''
-class_op, softmax_op, pred_op = generate_full_model(input_placeholder, weights, biases)
+m = Model()
+class_op, softmax_op, pred_op = m.generate_full_model(input_placeholder, weights, biases)
 
 loss = tf.losses.sparse_softmax_cross_entropy(label_ph, pred_op)
 opt = tf.train.AdamOptimizer()
@@ -190,6 +193,7 @@ with tf.Session() as sess:
 	
 
 	print("printing gradients:")
+	m.ranks
 	#print(gr)
 	#for g in gr:
 	#	print(g.shape)
