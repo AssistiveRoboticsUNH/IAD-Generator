6de84a4a70425ffa4a66a6a21cadd1cd1e5c9ba0
MClarkTurner <mclarktu@fandm.edu>
2019-09-03 21:40:39 -0400

    test

---
+++ iad-generation/modify_gradient_example.py
@@ -0,0 +1,21 @@
+import tensorflow as tf
+
+input = tf.Variable([3.0], dtype=tf.float32)
+
+@tf.custom_gradient
+def clip_grad_layer(x):
+  def grad(dy):
+    return tf.clip_by_value(dy, -0.1, 0.1)
+  return tf.identity(x), grad
+
+output_clip = clip_grad_layer(input)
+grad_clip = tf.gradients(output_clip, input)
+
+# output without gradient clipping in the backwards pass for comparison:
+output = tf.identity(input)
+grad = tf.gradients(output, input)
+
+with tf.Session() as sess:
+  sess.run(tf.global_variables_initializer())
+  print("with clipping:", sess.run(grad_clip)[0])
+  print("without clipping:", sess.run(grad)[0])
