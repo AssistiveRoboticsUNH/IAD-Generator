4c557558976b8d5ec984a4d5a8674f6c69c5e96b
MClarkTurner <mclarktu@fandm.edu>
2019-09-11 13:17:55 -0400

    ensemble.py

--- iad-generation/make_iads_i3d_02.sh
+++ iad-generation/make_iads_i3d_02.sh
@@ -3,9 +3,6 @@
 SPLIT=2
 SIZE=25
 
-python iad_generator.py "$MODEL" "$LIST_DIR"/trainlist0"$SPLIT"_"$SIZE".list --prefix "$OUT_DIR"/ucf_"$SIZE"_train
-python iad_generator.py "$MODEL" "$LIST_DIR"/testlist0"$SPLIT".list --prefix "$OUT_DIR"/ucf_"$SIZE"_test --min_max_file "$OUT_DIR"/min_maxes.npz
-	
-
+python iad_generator.py $MODEL $LIST_DIR/trainlist0$SPLIT_$SIZE.list --prefix $OUT_DIR/ucf_$SIZE_train
 
 
