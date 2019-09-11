be234e3f449bd27b9aef994f7299cbc3bb49aafb
MClarkTurner <mclarktu@fandm.edu>
2019-09-11 12:59:34 -0400

    ensemble.py

--- iad-generation/make_all_iads.sh
+++ iad-generation/make_all_iads.sh
@@ -3,8 +3,8 @@
 SPLIT=1
 SIZE=100
 
-echo "python iad_generator.py ~/i3d/train_i3d/experiments/ucf-101/models/ucf_i3d_pretrained_0$SPLIT_$SIZE ~/datasets/UCF-101/listFiles/trainlist0$SPLIT_$SIZE.list --prefix ucf_0$SPLIT/ucf_$SIZE/ucf_$SIZE_train"
-echo "python iad_generator.py ~/i3d/train_i3d/experiments/ucf-101/models/ucf_i3d_pretrained_0$SPLIT_$SIZE ~/datasets/UCF-101/listFiles/testlist0$SPLIT_$SIZE.list --prefix ucf_0$SPLIT/ucf_$SIZE/ucf_$SIZE_test --min_max_file ucf_0$SPLIT/ucf_$SIZE/min_maxes.npz"
+python iad_generator.py ~/i3d/train_i3d/experiments/ucf-101/models/ucf_i3d_pretrained_0$SPLIT_$SIZE ~/datasets/UCF-101/listFiles/trainlist0$SPLIT_$SIZE.list --prefix ucf_0$SPLIT/ucf_$SIZE/ucf_$SIZE_train
+python iad_generator.py ~/i3d/train_i3d/experiments/ucf-101/models/ucf_i3d_pretrained_0$SPLIT_$SIZE ~/datasets/UCF-101/listFiles/testlist0$SPLIT_$SIZE.list --prefix ucf_0$SPLIT/ucf_$SIZE/ucf_$SIZE_test --min_max_file ucf_0$SPLIT/ucf_$SIZE/min_maxes.npz
 	
 
 
