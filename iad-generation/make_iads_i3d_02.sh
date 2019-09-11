18cac87d6aed72e2473b74c5019ee6226fe1d72e
MClarkTurner <mclarktu@fandm.edu>
2019-09-11 13:18:55 -0400

    ensemble.py

--- iad-generation/make_iads_i3d_02.sh
+++ iad-generation/make_iads_i3d_02.sh
@@ -3,6 +3,9 @@
 SPLIT=2
 SIZE=25
 
-python iad_generator.py $MODEL $LIST_DIR/trainlist0$SPLIT_$SIZE.list --prefix $OUT_DIR/ucf_$SIZE_train
+MODEL="~/i3d/train_i3d/experiments/ucf-101/models/ucf_i3d_pretrained_0"$SPLIT"_"$SIZE
+LIST_DIR="~/datasets/UCF-101/listFiles"
+OUT_DIR="ucf_0"$SPLIT"/ucf_"$SIZE
+python iad_generator.py $MODEL $LIST_DIR/trainlist0$SPLIT_$SIZE.list --prefix $OUT_DIR/ucf_$SIZE
 
 
