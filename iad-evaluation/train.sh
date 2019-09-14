539e5d4c8ff7e5fb705b9bdc17ff270a0814875b
MClarkTurner <mclarktu@fandm.edu>
2019-09-14 01:03:16 -0400

    ensemble code

--- iad-evaluation/train.sh
+++ iad-evaluation/train.sh
@@ -5,7 +5,7 @@ do
 	IAD_DIR="../iad-generation/ucf_01/ucf_"$SIZE
 	LIST_DIR="~/datasets/UCF-101/listFiles/"
 
-	CMD="python ensemble.py model/test 101 "$IAD_DIR" --test "$LIST_DIR"testlist01.list --train "#LIST_DIR"trainlist01_"$SIZE".list"
+	CMD="python ensemble.py model/ucf_"$SIZE" 101 "$IAD_DIR" --test "$LIST_DIR"testlist01.list --train "#LIST_DIR"trainlist01_"$SIZE".list"
 	echo $CMD
 	#eval $CMD
 done
