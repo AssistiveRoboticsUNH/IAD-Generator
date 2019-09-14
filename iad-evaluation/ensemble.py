c638ff14bf9289d0fbd75b7df424866644182eb5
MClarkTurner <mclarktu@fandm.edu>
2019-09-14 12:32:04 -0400

    check for illegal path chars

--- iad-evaluation/ensemble.py
+++ iad-evaluation/ensemble.py
@@ -449,7 +449,10 @@ def test_model(model, test, num_classes):
 
             for j in range(len(data)):
                 for d in range(6):
-                    batch_data[ops['ph']["x_" + str(d)]] = np.expand_dims(data[d][j], axis=0)
+                    input_data = data[d][j]
+                    print("input_data.shape:", input_data.shape)
+
+                    batch_data[ops['ph']["x_" + str(d)]] = 
                 
                 result = sess.run([
                     ops['test_correct_pred'],
