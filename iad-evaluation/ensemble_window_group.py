90d4de56fcb60e26f5dd8fd66e090e3f67481a70
MClarkTurner <mclarktu@fandm.edu>
2019-09-15 19:27:13 -0400

    ensemble grouped

--- iad-evaluation/ensemble_window_group.py
+++ iad-evaluation/ensemble_window_group.py
@@ -537,14 +537,14 @@ def main():
 
     # define the dataset file names 
     eval_files = [x for x in os.listdir(args.iad_dir) if x.find("test") >= 0 and x.find("pruned") < 0]
-    eval_dataset = locate_iads(args.test) 
+    eval_dataset = locate_iads(eval_files) 
 
     if args.train != '':
         print("----> TRAINING")
         BATCH_SIZE = 15
 
         train_files = [x for x in os.listdir(args.iad_dir) if x.find("train") >= 0 and x.find("pruned") < 0]   
-        train_dataset = locate_iads(args.train)
+        train_dataset = locate_iads(train_files)
         train_model(args.model, train_dataset, eval_dataset, args.num_classes)
     elif args.test != '':
         print("----> TESTING")
