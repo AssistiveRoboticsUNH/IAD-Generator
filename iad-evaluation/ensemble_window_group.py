e3933cebcbfc47f6dc23349ee5c5f3bbf751ba3e
MClarkTurner <mclarktu@fandm.edu>
2019-09-15 20:08:54 -0400

    ensemble grouped

--- iad-evaluation/ensemble_window_group.py
+++ iad-evaluation/ensemble_window_group.py
@@ -208,14 +208,14 @@ def get_data_train(iad_list):
             #break d in to chuncks of window size
             window_size = input_shape[layer][1]
             pad_length = window_size - (z%window_size)
-            print("pre_pad: ", d.shape, z, pad_length, window_size)
+            #print("pre_pad: ", d.shape, z, pad_length, window_size)
             d = np.pad(d, [[0,0],[0,pad_length]], 'constant', constant_values=0)
-            print("pre_split: ", d.shape, d.shape[1], window_size, z)
+            #print("pre_split: ", d.shape, d.shape[1], window_size, z)
             d = np.split(d, d.shape[1]/window_size, axis=1)
             
             d = np.stack(d)
-            print("post_split: ", d.shape)
-            print('')
+            #print("post_split: ", d.shape)
+            #print('')
             file_data.append(d)
 
         flat_data = np.concatenate([x.reshape(x.shape[0], -1, 1) for x in file_data], axis = 1)
