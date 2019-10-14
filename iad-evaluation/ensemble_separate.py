0b19ffcfe083359aae22a45b6d862d05f5c878e3
MClarkTurner <mclarktu@fandm.edu>
2019-10-14 01:17:25 -0400

    cleaning up ensemble_separate

--- iad-evaluation/ensemble_separate.py
+++ iad-evaluation/ensemble_separate.py
@@ -350,8 +350,8 @@ def main(model_type, dataset_dir, csv_filename, num_classes, operation, dataset_
 	train_data = [ex for ex in csv_contents if ex['dataset_id'] <= dataset_id and ex['dataset_id'] > 0]
 	test_data  = [ex for ex in csv_contents if ex['dataset_id'] == 0] 
 
-	train_data = train_data[:5]
-	test_data = test_data[:5]
+	#train_data = train_data[:5]
+	#test_data = test_data[:5]
 
 
 	# Determine features to prune
