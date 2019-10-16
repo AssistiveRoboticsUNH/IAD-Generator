dad512df79da084babd9691fcf55bee47b94e4c1
MClarkTurner <mclarktu@fandm.edu>
2019-10-16 08:43:50 -0400

    add sliding_window

--- iad-evaluation/ensemble_separate.py
+++ iad-evaluation/ensemble_separate.py
@@ -310,6 +310,8 @@ def test_model(iad_model_path, model_dirs, num_classes, test_data, pruning_index
 			num_iter = len(test_data)
 			for i in range(num_iter):
 
+				aggregated_confidences[i].append([])
+
 				data, label = get_stack_data(test_data, model_num, pruning_indexes, input_shape, 1, batch_indexes=[i], sliding_window=sliding_window)
 				data = data[0]
 
@@ -330,7 +332,7 @@ def test_model(iad_model_path, model_dirs, num_classes, test_data, pruning_index
 					print("output:", confidences.shape)
 
 					# append confidences for evaluating consensus model
-					aggregated_confidences[i].append(confidences)
+					aggregated_confidences[i][model_num].append(confidences)
 					if(model_num == 0):
 						aggregated_labels.append(label)
 
