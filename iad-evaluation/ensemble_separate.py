b7fee938fc60348987524ffc9c17e02d8beaf9fc
MClarkTurner <mclarktu@fandm.edu>
2019-10-16 08:37:38 -0400

    add sliding_window

--- iad-evaluation/ensemble_separate.py
+++ iad-evaluation/ensemble_separate.py
@@ -323,7 +323,7 @@ def test_model(iad_model_path, model_dirs, num_classes, test_data, pruning_index
 					num_win = 1
 
 				for w_idx in range(num_win): # replace with len(data[0]) if using sliding window
-					#print(w_idx, num_win)
+					print(data[w_idx].shape, label.shape)
 					feed_dict = { ph["x_"+str(model_num)]: np.expand_dims(data[w_idx], axis = 0), ph["y"]: label,  ph["train"]: False }
 
 					confidences, predictions = sess.run([ 
