8dfcc08fcc35eb8640dc0abffa172a4e442a4bd4
MClarkTurner <mbc2004@cs.unh.edu>
2019-12-10 13:51:50 -0500

    feature rank

--- iad-generation/feature_rank_utils.py
+++ iad-generation/feature_rank_utils.py
@@ -59,9 +59,9 @@ def get_top_n_feature_indexes_combined(frames_file, flow_file, n):
 		# organize the ranks depending on whether they came from the frames dataset or 
 		# the flow dataset
 		print( (s_sub[:n]))
-		print( np.where(s_sub[:n] ==  0))
-		pruning_indexes["frames"].append(idx[ np.where(s_sub[:n] ==  0) ])
-		pruning_indexes["flow"].append(idx[ np.where(s_sub[:n] ==  1) ])
+		print( np.where(s_sub[:n] ==  0)[0])
+		pruning_indexes["frames"].append(idx[ np.where(s_sub[:n] ==  0)[0] ])
+		pruning_indexes["flow"].append(idx[ np.where(s_sub[:n] ==  1)[0] ])
 
 	return keep_sources, keep_indexes
 
