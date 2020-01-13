5c8f832ffc19deb2aeff77f5b26d6d808dbb1631
MClarkTurner <mclarktu@fandm.edu>
2020-01-13 00:35:42 -0500

    feature rank confirmation

--- iad-generation/rank_features.py
+++ iad-generation/rank_features.py
@@ -115,7 +115,7 @@ def weight_magnitudes(model_type, model_filename, pad_length, isRGB, gpu):
 		# prevent further modification to the graph
 		sess.graph.finalize()
 
-		w0 = sess.run(all_w[0])
+		w0 = sess.run(variables[all_w[0]])
 		print(w0)#, w0.shape)
 	
 
