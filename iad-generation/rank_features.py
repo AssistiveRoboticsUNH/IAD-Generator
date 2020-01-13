e95b15eb6cbfa77b56c49fbb2318c66f0e9a5c54
MClarkTurner <mclarktu@fandm.edu>
2020-01-12 23:49:37 -0500

    feature rank confirmation

--- iad-generation/rank_features.py
+++ iad-generation/rank_features.py
@@ -81,7 +81,7 @@ def weight_magnitudes(model_type, model_filename, pad_length, isRGB, gpu):
 
 	check_w = []
 
-	for v in variables.keys():
+	for v in sorted(variables.keys()):
 
 		#if (v in check_w):
 
