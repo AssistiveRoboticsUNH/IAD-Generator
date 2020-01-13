f5b774ef5a17dbfea28cffa4aa8aa9cdb4036474
MClarkTurner <mclarktu@fandm.edu>
2020-01-13 01:09:52 -0500

    feature rank confirmation

--- iad-generation/rank_features.py
+++ iad-generation/rank_features.py
@@ -108,7 +108,7 @@ def weight_magnitudes(model_type, model_filename, dataset_dir, csv_filename, dat
 		#all_w[i] = [tf.reduce_sum(tf.math.abs( v ), axis=[0,1,2,3]) for v in all_w[i]]
 		
 		# L2-norm
-		all_w[i] = [tf.reduce_sum(tf.norm( v, ord=2 ), axis=[0,1,2,3]) for v in all_w[i]]
+		all_w[i] = [tf.reduce_sum(tf.math.l2_normalize( v ), axis=[0,1,2,3]) for v in all_w[i]]
 
 		all_w[i] = tf.concat(all_w[i], axis=0)
 
