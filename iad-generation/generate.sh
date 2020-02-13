bae3b5879e21bcee1c539365472b37967d1eb3bd
MClarkTurner <mclarktu@fandm.edu>
2020-02-13 14:58:44 -0500

    tsm wrapper

--- iad-generation/generate.sh
+++ iad-generation/generate.sh
@@ -5,5 +5,5 @@ python iad_generator_flex.py \
 	~/datasets/Something-Something/ss.csv \
 	174 \
 	1 \
-	~/datasets/Something-Something/iad_frames_1/feature_ranks.npz \
+	~/datasets/Something-Something/iad_frames_1/feature_ranks_1.npz  \
 	100 
