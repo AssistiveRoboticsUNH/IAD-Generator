a7d5c2cb596d490b950095d207231b7367ccecbe
MClarkTurner <mclarktu@fandm.edu>
2020-02-13 14:15:20 -0500

    tsm wrapper

---
+++ iad-generation/rank.sh
@@ -0,0 +1,7 @@
+python rank_features.py \
+	tsm \
+	~/temporal-shift-module/TSM_somethingv2_RGB_resnet101_shift8_blockres_avg_segment8_e45.pth \
+	~/datasets/Something-Something/ \
+	~/datasets/Something-Something/ss.csv \
+	174 \
+	1 
