f3e3b0422bbc9a8cb31bc4ac3ff0976dffb871b7
MClarkTurner <mclarktu@fandm.edu>
2020-02-12 21:17:01 -0500

    tsm check

--- iad-generation/tsm_wrapper.py
+++ iad-generation/tsm_wrapper.py
@@ -61,7 +61,17 @@ class TSMBackBone(BackBone):
         test_file = None
 
         #model variables
+        def parse_shift_option_from_log_name(log_name):
+            if 'shift' in log_name:
+                strings = log_name.split('_')
+                for i, s in enumerate(strings):
+                    if 'shift' in s:
+                        break
+                return True, int(strings[i].replace('shift', '')), strings[i + 1]
+            else:
+                return False, None, None
         self.is_shift, shift_div, shift_place = parse_shift_option_from_log_name(this_weights)
+        
         
         self.arch = this_weights.split('TSM_')[1].split('_')[2]
         modality = 'RGB'
