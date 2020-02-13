0f6aab184f8fa223d2725f25db48975fd5d90fc6
MClarkTurner <mclarktu@fandm.edu>
2020-02-13 10:57:06 -0500

    tsm wrapper

--- iad-generation/tsm_wrapper.py
+++ iad-generation/tsm_wrapper.py
@@ -186,7 +186,7 @@ class TSMBackBone(BackBone):
             print("here1")
             def hook(model, input, output):
                 # perform taylor expansion
-                print("here2, ", len(input))
+                print("here2", input)
                 grad = input.detach()
 
                 self.ranks.append( grad ) 
