c345fe26f22379cbf6267790c8bce6c40edeb9d2
MClarkTurner <mclarktu@fandm.edu>
2020-02-13 23:08:46 -0500

    tsm wrapper

--- iad-generation/generate.sh
+++ iad-generation/generate.sh
@@ -2,7 +2,7 @@
 
 python iad_generator_flex.py \
 	tsm \
-	~/temporal-shift-module/TSM_somethingv2_RGB_resnet101_shift8_blockres_avg_segment8_e45.pth \
+	~/models/TSM_somethingv2_RGB_resnet101_shift8_blockres_avg_segment8_e45.pth \
 	~/datasets/Something-Something/ \
 	~/datasets/Something-Something/ss.csv \
 	174 \
