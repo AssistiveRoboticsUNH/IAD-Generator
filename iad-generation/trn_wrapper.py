6c6c537575196a8f2d086fa54d17305962bbdf94
MClarkTurner <mclarktu@fandm.edu>
2020-02-13 23:53:03 -0500

    trn wrapper

--- iad-generation/trn_wrapper.py
+++ iad-generation/trn_wrapper.py
@@ -138,11 +138,12 @@ class TRNBackBone(BackBone):
         #checkpoint_file = TRN_somethingv2_RGB_BNInception_TRNmultiscale_segment8_best.pth.tar
 
         crop_fusion_type = 'TRNmultiscale'
-        net = TSN(num_class, self.max_length, args.modality,
+        net = TSN(self.num_class, self.max_length, args.modality,
                   base_model=self.arch,
                   consensus_type=crop_fusion_type,
                   img_feature_dim=256,
                   )
+        print(net.base_model)
 
         # load checkpoint file
         checkpoint = torch.load(this_weights)
