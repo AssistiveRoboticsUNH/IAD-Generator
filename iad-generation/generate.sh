bf398ee15cc5524a9b9a9380ec83e9dabf0daf45
MClarkTurner <mclarktu@fandm.edu>
2020-02-13 22:23:23 -0500

    tsm wrapper

--- iad-generation/generate.sh
+++ iad-generation/generate.sh
@@ -1,14 +1,5 @@
-python iad_generator_flex.py \
-	tsm \
-	~/temporal-shift-module/TSM_somethingv2_RGB_resnet101_shift8_blockres_avg_segment8_e45.pth \
-	~/datasets/Something-Something/ \
-	~/datasets/Something-Something/ss.csv \
-	174 \
-	1 \
-	~/datasets/Something-Something/iad_frames_1/feature_ranks_1.npz  \
-	100
 
-
+# develop IADs for test dataset, done in background
 python iad_generator_flex.py \
 	tsm \
 	~/temporal-shift-module/TSM_somethingv2_RGB_resnet101_shift8_blockres_avg_segment8_e45.pth \
@@ -16,5 +7,16 @@ python iad_generator_flex.py \
 	~/datasets/Something-Something/ss.csv \
 	174 \
 	0 \
+	~/datasets/Something-Something/iad_frames_1/feature_ranks_1.npz  \
+	100 &
+
+# develop IADs for training dataset
+python iad_generator_flex.py \
+	tsm \
+	~/temporal-shift-module/TSM_somethingv2_RGB_resnet101_shift8_blockres_avg_segment8_e45.pth \
+	~/datasets/Something-Something/ \
+	~/datasets/Something-Something/ss.csv \
+	174 \
+	1 \
 	~/datasets/Something-Something/iad_frames_1/feature_ranks_1.npz  \
 	100
