f6f3d174397427fee16ba0310293a2671fb721c3
MClarkTurner <mclarktu@fandm.edu>
2020-02-19 22:43:17 -0500

    trn wrapper

--- iad-generation/trn_rank.sh
+++ iad-generation/trn_rank.sh
@@ -1,8 +1,9 @@
-python rank_features.py \
+python rank_features_mt.py \
 	trn \
 	~/models/TRN_somethingv2_RGB_BNInception_TRNmultiscale_segment8_best.pth.tar \
 	~/datasets/Something-Something/ \
 	~/datasets/Something-Something/ss.csv \
 	174 \
 	1 \
-	--dataset_size=50000 \
+	--dataset_size=50 \
+	--num_procs=2 \
