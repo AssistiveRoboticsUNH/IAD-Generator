5b314e3bdda10d794141ab0de26de1df61881328
MClarkTurner <mclarktu@fandm.edu>
2020-02-19 21:39:28 -0500

    check shape

--- iad-generation/iad_generator_flex.py
+++ iad-generation/iad_generator_flex.py
@@ -88,7 +88,7 @@ def main(
 	lengths = np.array([ex["length"] for ex in csv_contents])
 
 	print(lengths.mean(), lengths.max(), lengths.min())
-	print(np.percentile(lengths, 75), np.percentile(lengths, 95))
+	print(np.percentile(lengths, 75), np.percentile(lengths, 98))
 
 	return
 
