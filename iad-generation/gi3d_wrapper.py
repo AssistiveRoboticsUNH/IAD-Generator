b9106d2cb19b49a70f0c61aa53afe2a68f3b37e2
MClarkTurner <mclarktu@fandm.edu>
2020-02-23 22:21:48 -0500

    gluon wrapper

--- iad-generation/gi3d_wrapper.py
+++ iad-generation/gi3d_wrapper.py
@@ -111,12 +111,16 @@ class I3DBackBone(BackBone):
 
 
         one_hot_target = mx.nd.one_hot(mx.nd.array([csv_input["label"]]), self.num_classes)
-        print("data_in:", one_hot_target.shape)
         # record gradient information
         with ag.record(train_mode=False):
             out = self.net(data_in)
+            print("out:", out.shape)
+            print("one_hot_target:", one_hot_target.shape)
 
-            loss = L(out, one_hot_target.copyto(mx.gpu(0)))
+            oht_g.copyto(mx.gpu(0))
+            print("one_hot_target_gpu:", oht_g.shape)
+
+            loss = L(out, oht_g)
         print("out:", out)
 
         # do backward pass
