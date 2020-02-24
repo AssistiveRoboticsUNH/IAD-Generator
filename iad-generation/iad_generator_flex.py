9e942080c0edc018e863d61895b10ee8c1f98ea2
MClarkTurner <mclarktu@fandm.edu>
2020-02-24 03:28:16 -0500

    gluon wrapper

--- iad-generation/iad_generator_flex.py
+++ iad-generation/iad_generator_flex.py
@@ -25,6 +25,7 @@ def convert_to_iad(data, csv_input, length_ratio, iad_data_path):
 		csv_input['iad_path_'+str(layer)] = os.path.join(label_path, csv_input['example_id'])+"_"+str(layer)+".npz"
 
 		data[layer] = data[layer][:, :int(data[layer].shape[1]*length_ratio)]
+		data[layer] = np.squeeze(data[layer])
 		print(data[layer].shape)
 
 		np.savez_compressed(csv_input['iad_path_'+str(layer)], data=data[layer], label=csv_input['label'], length=data[layer].shape[1])
