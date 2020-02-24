58428b4ebcea655543106d6e6c628d4645b57322
MClarkTurner <mbc2004@cs.unh.edu>
2020-02-24 03:30:06 -0500

    gi3d

--- iad-generation/gi3d_generate.sh
+++ iad-generation/gi3d_generate.sh
@@ -2,11 +2,11 @@
 
 python iad_generator_flex.py \
 	i3d \
-	~/models/TRN_somethingv2_RGB_BNInception_TRNmultiscale_segment8_best.pth.tar \
+	'' \
 	~/datasets/Something-Something/ \
 	~/datasets/Something-Something/ss.csv \
 	174 \
 	1 \
 	~/datasets/Something-Something/iad_i3d_frames_1/feature_ranks_1.npz  \
-	10 \
-	--num_procs=1
+	70 \
+	--num_procs=4
