2828c5932426e210244c14989efa391bc11252c2
MClarkTurner <mclarktu@fandm.edu>
2020-02-25 11:07:02 -0500

    py3 format

--- iad-generation/backbone_wrapper.py
+++ iad-generation/backbone_wrapper.py
@@ -1,18 +1,20 @@
+DEPTH = 0
+CNN_FEATURE_COUNT = []
 class BackBone:
 
-    def open_model(self, max_length=8, start_idx=0):
-        pass
+	def open_model(self, max_length=8, start_idx=0):
+		pass
 
-    def predict(self, csv_input, max_length=1):
-        pass
+	def predict(self, csv_input, max_length=1):
+		pass
 
-    def process(self, csv_input, max_length=1):
-        pass
-        #return iad_data, length_ratio
+	def process(self, csv_input, max_length=1):
+		pass
+		#return iad_data, length_ratio
 
-    def rank(self, csv_input, max_length=1):
-        pass
-        #return rank_data
+	def rank(self, csv_input, max_length=1):
+		pass
+		#return rank_data
 
-    def __init__(self, checkpoint_file, num_classes, feature_idx=None):
-		self.CNN_FEATURE_COUNT = []
+	def __init__(self, checkpoint_file, num_classes, feature_idx=None):
+		self.num_classes = num_classes
