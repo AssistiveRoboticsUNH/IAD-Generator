69723e012a344f56b0706aa2ae31d3281f88c433
MClarkTurner <mclarktu@fandm.edu>
2020-02-26 10:42:17 -0500

    check gi3d

--- iad-generation/iad_generator_flex.py
+++ iad-generation/iad_generator_flex.py
@@ -22,23 +22,23 @@ class WorkerStopException(Exception):
 def convert_to_iad(data, csv_input, length_ratio, iad_data_path):
 	#save to disk
 	for layer in range(len(data)):
-		print("t0")
+		#print("t0")
 		label_path = os.path.join(iad_data_path, csv_input['label_name'])
 		if(not os.path.exists(label_path)):
 			os.makedirs(label_path)
-		print("t1")
+		#print("t1")
 		csv_input['iad_path_'+str(layer)] = os.path.join(label_path, csv_input['example_id'])+"_"+str(layer)+".npz"
 
-		print(data[layer].shape, length_ratio)
+		#print(data[layer].shape, length_ratio)
 		data[layer] = data[layer][:, :int(data[layer].shape[1]*length_ratio)]
-		print("t2", data[layer].shape)
+		#print("t2", data[layer].shape)
 		#data[layer] = np.squeeze(data[layer])
 		#print(data[layer].shape)
-		print("t3")
-		print(data[layer].shape)
+		#print("t3")
+		#print(data[layer].shape)
 
 		np.savez_compressed(csv_input['iad_path_'+str(layer)], data=data[layer], label=csv_input['label'], length=data[layer].shape[1])
-		print("t4")
+		#print("t4")
 
 def convert_dataset_to_iad(csv_contents, model, iad_data_path):
 	
@@ -46,19 +46,19 @@ def convert_dataset_to_iad(csv_contents, model, iad_data_path):
 	summed_ranks = None
 	# process files
 	for i, csv_ex in enumerate(csv_contents):
-		#try:
-		t_s = time.time()
+		try:
+			t_s = time.time()
 
-		# generate activation map
-		iad_data, length_ratio = model.process(csv_ex)
+			# generate activation map
+			iad_data, length_ratio = model.process(csv_ex)
 
-		# write the am_layers to file and get the minimum and maximum values for each feature row
-		convert_to_iad(iad_data, csv_ex, length_ratio, iad_data_path)
+			# write the am_layers to file and get the minimum and maximum values for each feature row
+			convert_to_iad(iad_data, csv_ex, length_ratio, iad_data_path)
 
-		print("converted video {:d} to IAD: {:6d}/{:6d}, time: {:8.2}".format(int(csv_ex['example_id']), i, len(csv_contents), time.time()-t_s))
-		#except:
-		#	print("Failed on file: ", csv_ex["example_id"])
-		#	raise WorkerStopException()
+			print("converted video {:d} to IAD: {:6d}/{:6d}, time: {:8.2}".format(int(csv_ex['example_id']), i, len(csv_contents), time.time()-t_s))
+		except:
+			print("Failed on file: ", csv_ex["example_id"])
+			raise WorkerStopException()
 
 def convert_csv_chunk(inputs):
 	csv_contents, model_type, model_filename, iad_data_path, num_classes, max_length, feature_idx = inputs
@@ -150,13 +150,11 @@ def main(
 		last += chunk_size
 
 	#convert files to IAD in parallel
-	convert_csv_chunk(inputs[0])
-	'''
+	#convert_csv_chunk(inputs[0])
 	try:
 		p.map(convert_csv_chunk, inputs)
 	except WorkerStopException:
 		sys.exit("generate failed")
-	'''
 	#summarize operations
 	print("--------------")
 	print("Summary")
