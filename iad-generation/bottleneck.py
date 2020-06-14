a9be91c668f97d80ead730ab69a46ee2142da228
MClarkTurner <mclarktu@fandm.edu>
2020-06-14 17:30:33 -0400

    bottleneck

--- iad-generation/bottleneck.py
+++ iad-generation/bottleneck.py
@@ -29,10 +29,8 @@ def train(csv_contents, model_type, model_filename, num_classes, dataset_id, iad
 	#model.train_model(csv_contents)
 	import tsm_wrapper3 as tw3 
 	for i in range(4):
-		tw3.validate(model, epoch=1)
 		tw3.train(model, epoch=1)#, log, tf_writer)
-	tw3.validate(model, epoch=1)
-
+		tw3.validate(model, epoch=1)
 
 
 def main(
