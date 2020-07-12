631ec996d926955d3cd4e54de0cc51f1518c5921
MClarkTurner <mclarktu@fandm.edu>
2020-07-12 10:01:55 -0400

    bottleneck fix

--- iad-generation/tsm_wrapper3.py
+++ iad-generation/tsm_wrapper3.py
@@ -179,10 +179,13 @@ class TSMBackBone(BackBone):
 
             #BatchNorm1d(2048, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
             #nn.ReLU(inplace=True)
-            nn.AdaptiveAvgPool2d(output_size=1),
+            nn.MaxPool2d(output_size=1),
+            #nn.AdaptiveAvgPool2d(output_size=1),
             #nn.Linear(2048, self.bottleneck_size)
 
         )
+        net.new_fc = nn.Linear(2048, self.bottleneck_size)
+
 
         
         if(not trim_net):
@@ -192,8 +195,7 @@ class TSMBackBone(BackBone):
             print("trim")
             net.consensus = nn.Identity()
             net.new_fc = nn.Identity()
-        net.new_fc = nn.Linear(2048, self.bottleneck_size)
-        
+
         net.base_model.fc = nn.Identity() # sets the dropout value to None
        # print(net) 
         
